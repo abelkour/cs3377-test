9ca1da5491edc627419a9bf8c7f69eac98b973db
Amine Belkoura <mxb135330@utdallas.edu>
2023-04-19 18:56:27 -0500

    initial version

---
+++ Helloworld.cpp
@@ -0,0 +1,16 @@
+// file: helloWorld.cpp
+// author: M. Amine Belkoura
+// date: 02/06/2015
+// purpose: CS3376
+// description:
+//   this prints a welcome message to the screen
+#include <iostream>
+using namespace std;
+int main( )
+{
+     cout <<" Hello from git" <<endl;
+     return 0;
+
+
+}
+
